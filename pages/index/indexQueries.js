17ca02e10b46610fbc95b4fb81d93cdd985fdf7a
kanghadong <hdkang2007@gmail.com>
2018-10-25 09:06:33 +0900

    make Search page and render products

--- pages/index/indexQueries.js
+++ pages/index/indexQueries.js
@@ -3,8 +3,26 @@ import { gql } from 'apollo-boost';
 export const INDEX_QUERY = gql`
   {
     categories {
-      id 
+      id
       name
+    }
+    onSale: products(where: { onSale: true }) {
+      id
+      name
+      detail
+      price
+      photo {
+        url
+      }
+    }
+    allProducts: products(where: { onSale: false }) {
+      id
+      name
+      detail
+      price
+      photo {
+        url
+      }
     }
   }
 `;
